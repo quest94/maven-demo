8416ac9406cab562cb9f6106370bd513da3eaba7
quest94 <quest94@163.com>
2024-01-08 18:28:30 +0800

    junit5测试

---
+++ junit5/src/test/java/org/quest94/demo/junit5/StandardTests.java
@@ -0,0 +1,44 @@
+package org.quest94.demo.junit5;
+
+import org.junit.jupiter.api.*;
+
+class StandardTests {
+
+    @BeforeAll
+    static void initAll() {
+        System.out.println("BeforeAll");
+    }
+
+    @BeforeEach
+    void init() {
+        System.out.println("BeforeEach");
+    }
+
+    @Test
+    void test1() {
+        System.out.println("test1");
+    }
+
+    @Test
+    void test2() {
+        System.out.println("test2");
+    }
+
+    @Test
+    @Disabled("for demonstration purposes")
+    void skippedTest() {
+        // not executed
+        System.out.println("skippedTest");
+    }
+
+    @AfterEach
+    void tearDown() {
+        System.out.println("AfterEach");
+    }
+
+    @AfterAll
+    static void tearDownAll() {
+        System.out.println("AfterAll");
+    }
+
+}
