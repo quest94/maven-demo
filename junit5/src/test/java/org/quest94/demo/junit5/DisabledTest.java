006627e8fe3f058477e7173899f153e3c8529c4b
quest94 <quest94@163.com>
2024-01-08 19:26:25 +0800

    忽略测试

---
+++ junit5/src/test/java/org/quest94/demo/junit5/DisabledTest.java
@@ -0,0 +1,14 @@
+package org.quest94.demo.junit5;
+
+import org.junit.jupiter.api.Disabled;
+import org.junit.jupiter.api.Test;
+
+public class DisabledTest {
+
+    @Disabled("忽略执行的描述")
+    @Test
+    void Demo() {
+        System.out.println("忽略执行");
+    }
+
+}
