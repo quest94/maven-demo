0e0232c0d515ea734cebb9155121a65130a431c8
quest94 <quest94@163.com>
2024-01-08 19:25:14 +0800

    重复测试

---
+++ junit5/src/test/java/org/quest94/demo/junit5/RepeatedTest.java
@@ -0,0 +1,14 @@
+package org.quest94.demo.junit5;
+
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.DisplayName;
+
+public class RepeatedTest {
+
+    @org.junit.jupiter.api.RepeatedTest(10)  // 表示重复执行10次
+    @DisplayName("重复测试")
+    public void testRepeated() {
+        Assertions.assertTrue(1 == 1);
+    }
+
+}
